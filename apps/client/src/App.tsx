8724c7c8ed4b50a874f0f4297ac8653a7b64a0cc
PabloJoan <iglesias.pablo10@gmail.com>
2026-01-22 13:55:21 -0500

    remove hardcoded backend url string in frontend

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -22,7 +22,7 @@ export function App() {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:4000/trpc",
+          url: `${import.meta.env.VITE_BACKEND_URL}/trpc`,
         }),
       ],
     }),
--- packages/shared/src/index.js
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-export const createFeedbackSchema = z.object({
-    title: z.string().min(1, "Title is required"),
-    description: z.string().min(1, "Description is required"),
-});
-//# sourceMappingURL=index.js.map
